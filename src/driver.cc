d5e1152709934b88f37e1da52e095ac63a6a407c
Srikriti Mutyala <mutyalasrikriti2006@gmail.com>
2026-02-27 14:41:03 -0600

    duplicate acc handling

--- src/driver.cc
+++ src/driver.cc
@@ -9,4 +9,6 @@ int main() {
   atm.DepositCash(12345678, 1234, 40000);
   atm.DepositCash(12345678, 1234, 32000);
   atm.PrintLedger("example.txt", 12345678, 1234);
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+
 }
--- tests/tests.cc
+++ tests/tests.cc
@@ -74,3 +74,14 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
+
+TEST_CASE("RegisterAccount throws on duplicate", "[register]") {
+  Atm atm;
+
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+
+  REQUIRE_THROWS_AS(
+      atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30),
+      std::invalid_argument
+  );
+}
